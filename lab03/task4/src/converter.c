9243c0b88bcd2b0eadfee6a8a9a9cad735be88be
dimik228 <73066202+dimik228@users.noreply.github.com>
2020-11-30 13:14:25 +0200

    Лабораторна робота №3

---
+++ lab03/task4/src/converter.c
@@ -0,0 +1,12 @@
+int main()
+{
+	#define uah 100; // Сума гривень 
+	const (double) usd = 28.27; // Доллар до гривні
+	const (double) eur = 33.33; // Евро до гривні
+	const (double) rub = 0.36;  // Буль до гривні
+
+	float uah_usd = UAH / usd; // Доллар до гривні 
+	float uah_eur = UAH / eur; // Евро до гривні 
+	float uah_rub = UAH / rub;// Рубль до гривні 
+	return 0 
+}
